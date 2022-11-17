66c33b1c7fee2b1dca7ac1073606b826b9298de2
HaidarFico <haidarficoi@gmail.com>
2022-11-17 18:44:55 +0700

    add

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas PWEB</title>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <a href="form.php"><button type="button">Klik untuk Mendaftar</button></a>
+    <!-- <a href="https://www.google.com"><button type="button">Klik untuk Mendaftar</button></a> -->
+</body>
+</html>
