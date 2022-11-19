b7d8373936a5e435942e24fabcfae2a694cf72e5
Ficoi <haidarficoi@gmail.com>
2022-11-19 17:32:08 +0700

    Reworked Version

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tugas PWEB</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran siswa baru.</h3>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Form Pendaftaran</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
---
+++ list-siswa.php
@@ -0,0 +1,12 @@
+<?php include("config.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tugas PWEB</title>
+</head>
+<body>
+    <header> <h1>Siswa yang sudah mendaftar</h1></header>
+    <nav><a href="form"
+</body>
+</html>
